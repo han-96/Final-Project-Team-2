72f25e76aa02e8ff5a03c5d45db1fd7c8e22828f
han-96 <hangvu.96@outlook.com>
2020-07-16 08:09:26 +0700

    Sign up

---
+++ demo.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `member` (
+        `password` VARCHAR(255) NOT NULL,
+        `email` VARCHAR(255) NOT NULL,
+        `fullname` VARCHAR(255) NOT NULL,
+        `birthday` VARCHAR(10) NOT NULL,
+        `sex` VARCHAR(3) NOT NULL
+    ) ENGINE = MyISAM'
